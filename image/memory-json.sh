050d0e50e2e7122bb99d238b2575b7ece7c29d6c
Samuel Colvin <s@muelcolvin.com>
2016-12-18 14:12:37 +0000

    correct mem percent calc

--- image/memory-json.sh
+++ image/memory-json.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
+# memory and CPU usage and print in json format.
+# this assumes `free` gives the old style output with a "-/+ buffers/cache:.." line,
+# it won't work with new style "available" `free`.
 set -e
 free -m > .mem
-mem_used=`grep "Mem" .mem | awk '{print $3}'`
-mem_percent=`grep "Mem" .mem | awk '{print $3/$2*100}'`
+mem_total=`grep "Mem" .mem | awk '{print $2}'`
+mem_used=`grep "buffers/cache" .mem | awk '{print $3}'`
+mem_percent=`echo "$mem_total $mem_used" | awk '{print $2/$1*100}'`
 cpu_percent=`mpstat | tail -n 1 | awk '{print $3+$4+$5}'`
 echo "{\"info\": \"sys\", \"mem_used\": $mem_used, \"mem_percent\": $mem_percent, \"cpu_percent\": $cpu_percent, \"run\": $1}"
 rm .mem

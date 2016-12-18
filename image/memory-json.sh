c6237acbef9f4d94910c103ca049a0ed33ea625b
Samuel Colvin <s@muelcolvin.com>
2016-12-18 14:22:12 +0000

    correct memory-json if mem can't be found

--- image/memory-json.sh
+++ image/memory-json.sh
@@ -8,5 +8,10 @@ mem_total=`grep "Mem" .mem | awk '{print $2}'`
 mem_used=`grep "buffers/cache" .mem | awk '{print $3}'`
 mem_percent=`echo "$mem_total $mem_used" | awk '{print $2/$1*100}'`
 cpu_percent=`mpstat | tail -n 1 | awk '{print $3+$4+$5}'`
-echo "{\"info\": \"sys\", \"mem_used\": $mem_used, \"mem_percent\": $mem_percent, \"cpu_percent\": $cpu_percent, \"run\": $1}"
+unknown="\"-\""
+printf "{\"info\": \"sys\", "
+printf "\"mem_used\": ${mem_used:-$unknown}, "
+printf "\"mem_percent\": ${mem_percent:-$unknown}, "
+printf "\"cpu_percent\": ${cpu_percent:-$unknown}, "
+printf "\"run\": ${1:-$unknown}}\n"
 rm .mem

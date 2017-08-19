ad568575d959cd8b27782b1079b54cc2cacfe147
Samuel Colvin <s@muelcolvin.com>
2017-08-19 20:22:11 +0100

    add name to container

--- create-container.sh
+++ create-container.sh
@@ -9,4 +9,5 @@ docker run -d -p=25:25 -p=465:465 -p=587:587 --restart unless-stopped \
 --log-opt awslogs-region=$AWS_DEFAULT_REGION \
 --log-opt awslogs-group=DockerPostfix \
 --log-opt awslogs-stream=docker-postfix \
+--name postfix \
 samuelcolvin/postfix-forward
